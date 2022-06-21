1df29a01047e2fc404f4a46e6e864293b6ae14fd
BrianMakanjira <makanjirabrian@gmail.com>
2022-06-21 15:31:07 +0200

    1 file modified

--- src/components/ToDos.vue
+++ src/components/ToDos.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
 <h1>My To Do List</h1>
+<ul>
+<li v-bind:key="item.id" v-for="item in todoEntries">
+<ToDoItem v-bind:todoItem="item" />
+</li>
+</ul>
 </div>
 </template>
 <script>
